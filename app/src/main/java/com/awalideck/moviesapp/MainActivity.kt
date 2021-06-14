5c9be5b51d2acadb5df1d046ac7b8968f9c6e27a
davidwekesar <davidwekesar@gmail.com>
2021-06-14 15:03:24 +0300

    Add retrofit and navigation dependencies

--- app/src/main/java/com/awalideck/moviesapp/MainActivity.kt
+++ app/src/main/java/com/awalideck/moviesapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.awalideck.moviesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.awalideck.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
