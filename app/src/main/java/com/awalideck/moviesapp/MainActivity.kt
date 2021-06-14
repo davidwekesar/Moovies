8817c33ee4ea680991fd4898ba61f4433f0d43d0
davidwekesar <davidwekesar@gmail.com>
2021-06-14 15:42:37 +0300

    Add Search fragment

--- app/src/main/java/com/awalideck/moviesapp/MainActivity.kt
+++ app/src/main/java/com/awalideck/moviesapp/MainActivity.kt
@@ -6,8 +6,12 @@ import com.awalideck.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
